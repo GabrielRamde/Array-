5fedf75778572340075519c03ff9a594ddf12283
Gabriel Ramde <gara02001@utb.vaxjo.se>
2019-10-01 13:49:14 +0200

    Kodade lite

---
+++ Array/Array/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[5];
+            for (int i = 0; i < n.GetLength(0); i++)
+            {
+                Console.WriteLine("Skriv in 1 tal: ");
+                n[i] = int.Parse(Console.ReadLine());
+            }
+                  
+
+                {
+                for 
+
+                }
+        }
+    }
+}
+
